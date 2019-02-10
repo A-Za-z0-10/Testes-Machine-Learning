c451925b249e386e24a3b1f8da372871924d02c7
Daniel <danielmartinslopes9@gmail.com>
2019-02-10 20:13:16 -0200

    alt arquivos

--- time_db.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import datetime as dt
-from datetime import datetime
-
-def last_update(formato='%H:%M:%S %d/%m'):
-	#Retorna tempo atual do banco
-
-	return dt.datetime.now().strftime(formato)
-	
